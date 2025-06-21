206e728c98082cdf067fa4c14385f282ac1c8b2d
shkelzenkonxheli <xeni_kongjeli@hotmail.com>
2025-06-22 00:35:17 +0200

    Export changed

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -41,5 +41,4 @@ export const authOptions: NextAuthOptions = {
 
 const handler = NextAuth(authOptions);
 
-export const GET = handler;
-export const POST = handler;
+export { handler as GET, handler as POST };
