70b3e4c6bbf79ce4e6932d4ad7d24e63c5a761b1
shkelzenkonxheli <xeni_kongjeli@hotmail.com>
2025-06-22 01:28:52 +0200

    Uodated authOptions

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -41,4 +41,5 @@ export const authOptions: NextAuthOptions = {
 
 const handler = NextAuth(authOptions);
 
-export { handler as GET, handler as POST };
+export const GET = handler;
+export const POST = handler;
